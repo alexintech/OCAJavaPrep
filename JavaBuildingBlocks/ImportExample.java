87f4c707c533d1625351178666c3597af411d2e3
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-08-27 13:30:06 +0500

    Fixed import statements

--- JavaBuildingBlocks/ImportExample.java
+++ JavaBuildingBlocks/ImportExample.java
@@ -1,6 +1,8 @@
+import java.util.Random; // or
+// import java.util.*;
 public class ImportExample {
   public static void main(String[] args) {
-    Random r = new Random(); // DOES NOT COMPILE
+    Random r = new Random();
     System.out.println(r.nextInt(10)); 
   }
 }
