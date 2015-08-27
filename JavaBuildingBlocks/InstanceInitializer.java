9071ea156a7634becf9919d9682d6c40aa242f71
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-08-27 15:42:39 +0500

    You can use more than one instance initializer.

--- JavaBuildingBlocks/InstanceInitializer.java
+++ JavaBuildingBlocks/InstanceInitializer.java
@@ -1,8 +1,10 @@
 public class InstanceInitializer {
-  { System.out.println(name); }
   private String name = "Instance";
-
+  
+  { System.out.println(name); }
   public static void main(String[] args) {
     InstanceInitializer i = new InstanceInitializer();
   }
+  
+  { System.out.println("Second init"); }
 }
