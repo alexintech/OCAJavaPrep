a7e95148254aed9a1727e0ea91397f488d81cf7b
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-10-05 12:21:49 +0500

    Local object reference variable do not have any value (ever null) before initialization. You will get error when you have not initialized a variable:
    TimeTravel.java:5: error: variable date might not have been initialized
        if (date == null)
           ^

---
+++ JavaBuildingBlocks/TimeTravel.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+public class TimeTravel {
+  public static void main(String[] args) {
+    Date date;
+    if (date == null)
+      System.out.println("date is null");
+  }
+}
