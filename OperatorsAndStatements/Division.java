2d6b00f4e834ea6c0dea8cd5d763c9f1ad61a718
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-10-09 14:30:50 +0500

    Examples about division and modulus operators

---
+++ OperatorsAndStatements/Division.java
@@ -0,0 +1,11 @@
+public class Division {
+  public static void main(String[] args) {
+    System.out.println("11 / 3 = " + (11 / 3));
+    System.out.println("-11 / 3 = " + (-11 / 3));
+    System.out.println("11 % 3 = " + (11 % 3));
+    System.out.println("-11 % 3 = " + (-11 % 3));
+
+    int a = 10;
+    //! (++a)++; // error: ++ requires variable, but value is found
+  }
+}
