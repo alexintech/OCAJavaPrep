d051bfdc895a4670b9bdc4b54b03ea281ae531b1
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-10-13 16:49:33 +0500

    &, ^, | operators have higher precedence than &&, || operators.

---
+++ OperatorsAndStatements/Logical.java
@@ -0,0 +1,9 @@
+public class Logical {
+  public static void main(String[] args) {
+    int y = 5;
+    int x = 2;
+    if ((x > 3) && (y < 2) | true ) {
+      System.out.println("true");
+    }
+  }
+}
