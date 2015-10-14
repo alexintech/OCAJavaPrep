32029824e2e27fd10d78275ef51f4412be6b9300
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-10-14 12:49:07 +0500

    Boxing and unboxing examples

--- OperatorsAndStatements/Assignment.java
+++ OperatorsAndStatements/Assignment.java
@@ -8,5 +8,22 @@ public class Assignment {
       System.out.print(elem + " ");
     }
     System.out.println();
+
+    Boolean boolRef = true;     // boxing
+    Byte bRef = 2;              // constant in range: narrowing to byte, then boxing
+    //! Byte bRef2 = 257;           // constant not in range: cast required
+    //! Integer iRef3 = (short)10;  // short not assignable to Integer
+
+    short s = 10;           // narrowing
+    //! Integer iRef1 = s;      // short not assignable to Integer
+
+    //! Integer iRef1 = 'a';        // not compatible types
+
+    boolean bv1 = boolRef;  // unboxing
+    byte b1 = bRef;         // unboxing
+
+    Integer iRefVal = null;     // always allowed
+    int j = iRefVal;            // NullPointerException!
+    if (iRefVal != null) j = iRefVal;   // avoids the exception
   }
 }
