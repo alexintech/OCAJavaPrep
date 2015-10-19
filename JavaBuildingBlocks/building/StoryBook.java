be7612b8d3673ada477721226e956e70f8120187
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-10-19 18:42:33 +0500

    You can access protected members over the reference of the same class.

--- JavaBuildingBlocks/building/StoryBook.java
+++ JavaBuildingBlocks/building/StoryBook.java
@@ -6,7 +6,12 @@ public class StoryBook extends Book {
     author = "ABC";
     modifyTemplate();
     Book book = new Book();
-    String v = book.author;   // Compile error
-    book.modifyTemplate();    // Compile error
+    //! String v = book.author;   // Compile error
+    //! book.modifyTemplate();    // Compile error
+  }
+  public void refStoryBook() {
+    StoryBook sb = new StoryBook();
+    sb.modifyTemplate();  // OK, because sb is a reference to this class
+    String v = sb.author;
   }
 }
