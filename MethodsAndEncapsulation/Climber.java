8a90ab6fdf6ee457039db3b9e723958597f21707
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-10-23 01:17:25 +0500

    Compile-time error in lambda expression

---
+++ MethodsAndEncapsulation/Climber.java
@@ -0,0 +1,16 @@
+interface Climb {
+  boolean isTooHigh(int height, int limit);
+}
+
+public class Climber {
+  public static void main(String[] args) {
+    check((h, l) -> h.append(l).isEmpty(), 5); // error: int cannot be dereferenced
+  }
+  private static void check(Climb climb, int height) {
+    if (climb.isTooHigh(height, 10))
+      System.out.println("too high");
+    else
+      System.out.println("ok");
+  }
+}
+
