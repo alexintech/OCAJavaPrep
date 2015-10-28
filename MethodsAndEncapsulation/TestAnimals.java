885a453dc055b385d09692e0fad486d2430cb272
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-10-28 17:51:53 +0500

    Examples of illegal method overrides

--- MethodsAndEncapsulation/TestAnimals.java
+++ MethodsAndEncapsulation/TestAnimals.java
@@ -4,6 +4,9 @@ public class TestAnimals {
     Animal b = new Horse();
     a.eat();
     b.eat();
+    Horse h = new Horse();
+    h.jump();         // ok
+    //! b.jump();     // error: unreported exception; must be caught
   }
 }
 
@@ -11,12 +14,39 @@ class Animal {
   public void eat() {
     System.out.println("Generic Animal Eating Generically");
   }
+  public static void doStuff() { }
+
+  public void jump() throws Exception { }
 }
 
 class Horse extends Animal {
-  // error: attempting to assign weaker access privileges; was public
+  /* error: attempting to assign weaker access privileges; was public
   private void eat() {
     System.out.println("Horse eating hay, oats, "
                         + "and horse treats");
   }
+  */
+
+  // legal overriding
+  public void eat() {
+    System.out.println("Horse eating hay, oats, "
+                        + "and horse treats");
+  }
+
+  /* error: overridden method does not throw IOException
+  public void eat() throws IOException { }
+  */
+
+  /* error: return type String is not compatible with void
+  public String eat() { return "Horse"; }
+  */
+
+  // static methods are not overriden
+  public static void doStuff() {
+    //! super.doStuff();  // error: non-static variable super
+                          // cannot be referenced from a static context
+  }
+
+  // that's legal
+  public void jump() { /* no Exceptions */ }
 }
