cfe7d92e413fc71383f5d7e754dde6dcd69efb65
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-10-28 18:57:22 +0500

    The reference type determines which overloaded method is invoked.

--- MethodsAndEncapsulation/TestAnimals.java
+++ MethodsAndEncapsulation/TestAnimals.java
@@ -10,6 +10,25 @@ public class TestAnimals {
   }
 }
 
+class UseAnimals {
+  public void doStuff(Animal a) {
+    System.out.println("In the Animal version");
+  }
+  public void doStuff(Horse a) {
+    System.out.println("In the Horse version");
+  }
+  public static void main(String[] args) {
+    UseAnimals ua = new UseAnimals();
+    Animal animalObj = new Animal();
+    Horse horseObj = new Horse();
+    ua.doStuff(animalObj);
+    ua.doStuff(horseObj);
+
+    Animal animalRefToHorse = new Horse();
+    ua.doStuff(animalRefToHorse); // doStuff(Animal) version
+  }
+}
+
 class Animal {
   public void eat() {
     System.out.println("Generic Animal Eating Generically");
