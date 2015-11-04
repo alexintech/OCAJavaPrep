30d00134a51e8b9c1deb2fc5ac435c53a2864673
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-11-04 20:44:15 +0500

    Array declaration and initialization

---
+++ MethodsAndEncapsulation/Arrays.java
@@ -0,0 +1,24 @@
+public class Arrays {
+  static int[] testInit; // init with null
+
+  public static void main(String[] args) {
+    int[] localInit; // not initialized
+    System.out.println(testInit);
+    //! System.out.println(localInit);
+
+    int anIntArray[], oneInteger;
+    anIntArray = new int[0];
+    //! anIntArray = new int[-1]; // Exception NegativeArraySizeException
+    oneInteger = 5;
+
+    System.out.println("AHA".toCharArray()[1]);
+
+    int[] abc = {1, 2, 3, };
+    System.out.println(abc.length);
+
+    int[] a;
+    //! a = {1, 2, 3}; // not an expression. It doesn't return value
+    a = new int[] {1, 2, 3}; // anonymous array
+    //! a = new int[3] {1, 2, 3}; // error syntax
+  }
+}
