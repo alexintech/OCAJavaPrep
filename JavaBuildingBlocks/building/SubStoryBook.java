880024475b2c4136840dcd681e6c3e54dcc3b73a
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-11-11 15:23:55 +0500

    Final variable won't get default values.

--- JavaBuildingBlocks/building/SubStoryBook.java
+++ JavaBuildingBlocks/building/SubStoryBook.java
@@ -2,6 +2,8 @@ package building;
 import library.Book;
 
 public class SubStoryBook extends StoryBook {
+  //! public final static int count;  // count is not initialized.
+                                  // It will not get default value
   void action(Book book, StoryBook storyBook, SubStoryBook subStoryBook) {
     author = "Author";
     //! book.author = "Author";
