b6469805308ea8e4ce31e3f12efad3ce1701bf1a
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-11-22 17:03:02 +0500

    Using ArrayList methods

---
+++ CoreJavaAPIs/UsingArrayList.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class UsingArrayList {
+  public static void main(String[] args) {
+    // warning: Unchecked or unsafe operations
+    ArrayList list = new ArrayList();
+    list.add("hawk");
+    list.add(Boolean.TRUE);
+    list.add(12);
+    System.out.println(list);
+
+    ArrayList<String> safer = new ArrayList<>();
+    safer.add("sparrow");
+    //! safer.add(Boolean.TRUE);    // error: Boolean is not a String
+
+    List<String> birds = new ArrayList<>();
+    birds.add("hawk");
+    birds.add(1, "robin");
+    birds.add(0, "blue jay");
+    birds.add(1, "cardinal");
+    System.out.println(birds); // [blue jay, cardinal, hawk, robin]
+
+    birds.add("hawk");
+    System.out.println(birds.remove("blue")); // false
+    System.out.println(birds.remove("hawk")); // true
+    System.out.println(birds.remove(0)); // blue jay
+    System.out.println(birds); // [cardinal, robin, hawk]
+
+    System.out.println(birds.set(0, "robin")); // cardinal
+    System.out.println(birds.size()); // 3
+    // System.out.println(birds.set(9, "robin")); // throws IndexOutOfBoundsException
+
+    System.out.println(birds.isEmpty()); // false
+    System.out.println(birds.size()); // 3
+    birds.clear();
+    System.out.println(birds.isEmpty()); // true
+    System.out.println(birds.size()); // 0
+
+    birds.add("hawk");
+    System.out.println(birds.contains("hawk")); // true
+    System.out.println(birds.contains("robin")); // false
+
+    List<String> one = new ArrayList<>();
+    List<String> two = new ArrayList<>();
+    System.out.println(one.equals(two)); // true
+    one.add("a");
+    System.out.println(one.equals(two)); // false
+    two.add("a");
+    System.out.println(one.equals(two)); // true
+    one.add("b");
+    two.add(0, "b");
+    System.out.println(one.equals(two)); // false
+  }
+}
+
