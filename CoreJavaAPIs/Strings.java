23cad1410655d779048c3fa2e5bc35f8b0d035c8
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-12-01 15:37:58 +0500

    Examples of strings

--- CoreJavaAPIs/Strings.java
+++ CoreJavaAPIs/Strings.java
@@ -11,6 +11,13 @@ public class Strings {
     StringBuilder sb2 = new StringBuilder("0123456");
     System.out.println(sb2.subSequence(2, 4));
     System.out.println(sb2);
+
+    System.out.println(String.valueOf(new char[]{'a', 'h', 'a'}));
+    System.out.println(new char[]{'a', 'h', 'a'});
+
+    StringBuilder test = new StringBuilder("Some string");
+    test.setLength(3);    // Truncates string
+    System.out.println(test);  // Som
   }
 }
 
