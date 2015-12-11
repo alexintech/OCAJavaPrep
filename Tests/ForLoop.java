39ec8bdc599460666ca89f6213c28c34a2f693fd
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-12-11 21:14:36 +0500

    Using statements in the third part of for loop

---
+++ Tests/ForLoop.java
@@ -0,0 +1,16 @@
+public class ForLoop {
+  public static void main(String[] args) {
+    // for(;;) {
+    //!   Math.random()<.05? break : continue; // error: must return value
+    // }
+
+    //! for(;;Math.random()<0.5) {   // error: not a statement
+    //    System.out.println("true");
+    // }
+
+    Math.random();
+    //! Math.random()<0.5;   // error: not a statement
+
+  }
+}
+
