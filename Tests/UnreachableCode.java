4b592555dcdc66092fddb413a7cca8363a5cd7b9
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-12-12 20:48:34 +0500

    Unreachable statements in a loop with using continue statement in else clause

---
+++ Tests/UnreachableCode.java
@@ -0,0 +1,16 @@
+public class UnreachableCode {
+  static String[] days = {"monday", "tuesday", "wednesday"};
+
+  public static void main(String[] args) {
+    int index = 0;
+    for(String day : days) {
+      if (index == 3) {
+        break;
+      } else {
+        continue;  // makes following code unreachable
+      }
+      //! index++;  // unreachable statement
+    }
+  }
+}
+
