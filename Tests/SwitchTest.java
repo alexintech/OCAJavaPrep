18b8e4fce50d2f7cfdd2abb8c1d6e2fc148a74fc
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-12-13 14:20:56 +0500

    Char literal value is assignable to byte

---
+++ Tests/SwitchTest.java
@@ -0,0 +1,14 @@
+public class SwitchTest {
+  public static void main(String[] args) {
+    byte x = 5;
+    switch(x) {
+      case 'b':  // OK
+      default:
+      case -2:
+      case 80:
+    }
+
+    byte a = 'x';  // char is assignable to byte!
+  }
+}
+
