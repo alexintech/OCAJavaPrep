458833ca9ace0609ca340114ec45048de679a734
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-12-15 11:53:02 +0500

    Unary minus minimal int value will give the same value

--- Tests/Convertion.java
+++ Tests/Convertion.java
@@ -4,6 +4,10 @@ public class Convertion {
     float f = i;  // information was lost
     System.out.println(i - (int)f);  // -46
     System.out.println(f);
+
+    int a = Integer.MIN_VALUE;
+    int b = -a;
+    System.out.println(a + " " + b);
   }
 }
 
